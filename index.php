d00fe15969b64c4f80b26336c2579fa8770c7b32
Thiago Krug <thiagockrug@gmail.com>
2024-05-28 16:23:36 -0300

    começo do upload de arquivos

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload de Arquivo</title>
+    <title>Upload de Arquivos</title>
 </head>
 
 <body>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        Selecione o arquivo:<br>
-        <input type="file" name="arquivo"><br><br>
-        <input type="submit" value="Fazer Upload" name="submit">
+        Selecione o arquivo:
+        <input type="file" name="arquivo"><br>
+        <input type="submit" value="Enviar">
     </form>
 </body>
 
