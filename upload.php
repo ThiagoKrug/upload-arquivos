7e7bf4360b841c23cfb824d00992f78cd1472d4b
Thiago Krug <thiagockrug@gmail.com>
2024-06-04 17:19:09 -0300

    aula de 04/06

--- upload.php
+++ upload.php
@@ -1,43 +1,48 @@
 <?php
 $pastaDestino = "/uploads/";
-var_dump($_FILES);
 
-var_dump($_FILES['arquivo']['size']);
 // verificar se o tamanho do arquivo é maior que 2 MB
-if ($_FILES['arquivo']['size'] > 2000000) {
+if ($_FILES['arquivo']['size'] > 2000000) {  // condição de guarda 👮
     echo "O tamanho do arquivo é maior que o limite permitido. Limite máximo: 2 MB.";
     die();
 }
 
 // verificar se o arquivo é uma imagem
-//strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION))
-//var_dump($_FILES['arquivo']['name']);
-var_dump(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
+$extensao = strtolower(pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION));
 
+if (
+    $extensao != "png" && $extensao != "jpg" &&
+    $extensao != "jpeg" && $extensao != "gif" &&
+    $extensao != "jfif" && $extensao != "svg"
+) { // condição de guarda 👮
+    echo "O arquivo não é uma imagem! Apenas selecione arquivos 
+    com extensão png, jpg, jpeg, gif, jfif ou svg.";
+    die();
+}
 
+// verificar se é uma imagem de fato
+if (getimagesize($_FILES['arquivo']['tmp_name']) === false) {
+    echo "Problemas ao enviar a imagem. Tente novamente.";
+    die();
+}
 
+$nomeArquivo = uniqid();
 
-
-
+// se deu tudo certo até aqui, faz o upload
+$fezUpload = move_uploaded_file($_FILES['arquivo']['tmp_name'], 
+          __DIR__ . $pastaDestino . $nomeArquivo . "." . $extensao);
+if ($fezUpload == true) {
+    header("Location: index.php");
+} else {
+    echo "Erro ao mover arquivo.";
+}
 
 
 /*
-// pegamos o nome do arquivo
-$nomeArquivo = $_FILES['arquivo']['name'];
-
-var_dump($_FILES['arquivo']);
-
 // verificar se o arquivo já existe
 if (file_exists(__DIR__ . $pastaDestino . $nomeArquivo)) {
     echo "Arquivo já existe";
     exit;
 }
 //var_dump(__DIR__ . $pastaDestino . $nomeArquivo);
-
-var_dump(strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION)));
-$extensao = strtolower(pathinfo($nomeArquivo, PATHINFO_EXTENSION));
-if ($extensao != "jpg" && $extensao != "png" 
-    && $extensao != "jpeg" && $extensao != "gif") {
-    echo "O arquivo não é uma imagem";
-    exit;
 }*/
